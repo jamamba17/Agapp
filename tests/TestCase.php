759cd78dd2748ce2edfc00b2c2a5fa01f35e064d
jdice1771 <jdice1771@gmail.com>
2025-01-29 12:23:00 +0800

    add

---
+++ tests/TestCase.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests;
+
+use App\Models\Address\Address;
+use App\Models\User;
+use App\Models\UserProfile;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Routing\Middleware\ThrottleRequests;
+
+abstract class TestCase extends BaseTestCase
+{
+    use CreatesApplication;
+
+    public const BASE_API_URI = '/api/v1';
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // prevent throttling because we run test in parallel
+        $this->withoutMiddleware(ThrottleRequests::class);
+    }
+
+    /**
+     * Check if two arrays have the same value
+     */
+    protected function arraysHaveSameValue(array $arr1, array $arr2): bool
+    {
+        return (count($arr1) === count($arr2)) && ! array_diff($arr1, $arr2);
+    }
+
+    /**
+     * Generate required user info input
+     */
+    protected function getRequiredUserInputSample(): array
+    {
+        return [
+            'email' => fake()->unique()->safeEmail(),
+            'password' => 'Sample_Password_1',
+            'password_confirmation' => 'Sample_Password_1',
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+        ];
+    }
+
+    /**
+     * Create a certain number of users with UserProfile and Address factories
+     *
+     * @return Collection|Model
+     */
+    protected function produceUsers(int $quantity = 1, array $userAttr = [], bool $unVerified = false): Collection|User
+    {
+        $factory = User::factory()
+            ->has(
+                UserProfile::factory()->has(Address::factory())
+            )->count($quantity);
+
+        if ($unVerified) {
+            $factory = $factory->unVerified();
+        }
+
+        $users = $factory->create($userAttr);
+
+        if ($quantity === 1) {
+            return $users->first();
+        }
+
+        return $users;
+    }
+}
